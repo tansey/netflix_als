68a737c01aa240ef8134017c140b171e4b89c284
Wesley Tansey <wes.tansey@gmail.com>
2014-09-11 22:46:14 -0500

    Indentation fixes

--- netflix_als.py
+++ netflix_als.py
@@ -6,9 +6,9 @@ import argparse
 import scipy.io
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(description='Runs Alternating Least Squares (ALS) to learn latent features for users and movies.')
+    parser = argparse.ArgumentParser(description='Runs Alternating Least Squares (ALS) to learn latent features for users and movies.')
 
-	parser.add_argument('infile', help='The netflix dataset subset to use.')
+    parser.add_argument('infile', help='The netflix dataset subset to use.')
 
     parser.add_argument('--verbose', type=int, default=0, help='Print detailed progress information to the console. 0=none, 1=high-level info, 2=all details.')
     parser.add_argument('--solver', choices=['cf', 'cd'], default='cf', help='The method used to solve alternating least squares problem. cf=closed form. cd=coordinate descent.')
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     # Get the arguments from the command line
     args = parser.parse_args()
 
-	# Load data from file
-	data = scipy.io.loadmat(args.infile)
+    # Load data from file
+    data = scipy.io.loadmat(args.infile)
 
-	print 'matrix: {0}'.format(data)
+    print 'matrix: {0}'.format(data)
